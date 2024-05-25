73829913b6d17be90e2df687fc5eb65ae3084ac2
Roma-Leto <119625441+Roma-Leto@users.noreply.github.com>
2024-05-25 16:30:13 +0300

    STATIC_ROOT = 'static'

--- LHabrWeb/settings.py
+++ LHabrWeb/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = 'static'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
